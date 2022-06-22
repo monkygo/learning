29d05aac30c0f5abb8befaf9f66588c60acd41b9
monkygo <monkygo@users.noreply.github.com>
2022-06-22 03:56:06 -0700

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -1,3 +1,4 @@
+from platform import node
 from tkinter.messagebox import NO
 
 
@@ -18,17 +19,60 @@ class LinkedList:
             self.head = Node(data)
             return self.head
         else:
-            temp = self.head
             n = Node(data)
-            n.next = temp
+            n.next = self.head
             self.head = n
             return n
+    
+    def insertAt(self, data, i):
+        n = Node(data)
+
+        if self.head == None and i == 0:
+            self.head = n
+            return
+        
+        if i == 0:
+            n.next = self.head
+            self.head = n
+
+        curr = self.head
+        cnt = 0
+        while curr:
+            cnt += 1
+            prev = curr
+            curr = curr.next
+            if cnt == i:
+                n.next = curr
+                prev.next = n
+                break
+            
     
     def insertAfter(self, node, data):
         if node:
             n = Node(data)
             n.next = node.next
             node.next = n
+    
+    def append(self, data):
+        temp = self.head
+        last = temp
+        while temp:
+            last = temp
+            temp = temp.next
+        
+        n = Node(data)
+        if last:
+            last.next = n
+        else:
+            self.head = n
+
+    def length(self):
+        cnt = 0
+        temp = self.head
+        while temp:
+            cnt += 1
+            temp = temp.next
+        return cnt
 
     def print(self):
         # loop through list
@@ -37,14 +81,16 @@ class LinkedList:
         while n.next != None:
             #print(n.data)
             n = n.next
-            print(n.data)
+            print(n.data, end=" ")
         #print(n.data)
+        print("")
     
     def printList(self):
         temp = self.head
         while temp:
             print(temp.data, end=" ")
             temp = temp.next
+        print("")
 
 
 list = LinkedList(0)
@@ -53,7 +99,30 @@ list.insert(3)
 n4 = list.insert(4)
 list.insert(11)
 list.insertAfter(n4, 5)
-#list.print()
+list.append(99)
 list.printList()
+
+#list.print()
+#list.printList()
+
+list2 = LinkedList()
+list2.append(1)
+list2.append(2)
+list2.append(7)
+list2.printList()
+print("length:", list2.length())
+
+list3 = LinkedList()
+list3.insertAt(0, 0)
+list3.insertAt(10, 0)
+list3.insertAt(1, 1)
+list3.insertAt(2, 2)
+list3.insertAt(4, 2)
+list3.insertAt(3, 3)
+list3.insertAt(5, 5)
+list3.insertAt(7, 7)
+#list3.insertAt(5, 3)
+#print("missing head:", list3.head)
+list3.printList()
 
 
